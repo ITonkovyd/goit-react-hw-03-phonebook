ff9c18491a72c28da1c65c84b1bbe4cb7d08fbbf
Ihor Tonkovyd <142664318+ITonkovyd@users.noreply.github.com>
2023-08-30 16:34:04 +0300

    Add propType and change filtering

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,24 +4,32 @@ import { Component } from 'react';
 import { Ul } from './ContactList.styled';
 
 export default class ContactList extends Component {
-    
-
-
   render() {
-    const { contacts, handleDelete } = this.props
+    const { contacts, handleDelete } = this.props;
+
     return (
       <Ul>
-        {contacts.map((contact) => (
-          <ContactItem key={contact.id} id={contact.id} name={contact.name} number={contact.number} handleDelete={handleDelete} />
-        ))
-        }
+        {contacts.map(contact => (
+          <ContactItem
+            key={contact.id}
+            id={contact.id}
+            name={contact.name}
+            number={contact.number}
+            handleDelete={handleDelete}
+          />
+        ))}
       </Ul>
-    )
+    );
   }
 }
 
 ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  filteredContacts: PropTypes.array,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   handleDelete: PropTypes.func.isRequired,
-}
+};
